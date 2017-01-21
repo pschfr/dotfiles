6d359909a1611925c48888a0970e9722597e72c8
Paul Schaefer <yeppaul2@gmail.com>
2017-01-20 23:05:48 -0500

    removed extra newlines from status

--- custom/status.sh
+++ custom/status.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -e "\n\e[34m\e[1mPaul's Git Status Script v1.1\e[0m\n"
+echo -e "\e[34m\e[1mPaul's Git Status Script v1.1\e[0m"
 echo "Enter an absolute directory location, starting with /"
 echo "Press enter for default ($HOME/Dropbox/Work):"
 read -e ROOT_DIR # absolute location of directory containing multiple git repos
@@ -19,7 +19,7 @@ do
 	cd "$DIR"
 	if [ -d ".git" ]; then # if .git folder exists, check status
 		COMMITS=`git rev-list --all --count` # returns number of commits
-		echo -e "\n\e[34m\e[1m${DIR%?} \e[1;30m${COMMITS} commits\e[0m"
+		echo -e "\e[34m\e[1m${DIR%?} \e[1;30m${COMMITS} commits\e[0m"
 
 		if git status -s | read status; then # if modified, print status, otherwise up to date
 			git status -s
@@ -39,4 +39,4 @@ do
 	fi
 done
 
-echo -e "\n\e[34m\e[1m${REPOS}\e[0m repositories, \e[32m\e[1m${UPDATED}\e[0m up to date, and \e[31m\e[1m${MODIFIED}\e[0m with changes.\n"
+echo -e "\n\e[34m\e[1m${REPOS}\e[0m repositories, \e[32m\e[1m${UPDATED}\e[0m up to date, and \e[31m\e[1m${MODIFIED}\e[0m with changes."
